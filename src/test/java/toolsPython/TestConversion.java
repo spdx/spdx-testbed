c0cf3c3404225d30d861be18c017db600f67a081
Nicolaus Weidner <nicolaus.weidner@ververica.com>
2022-09-09 18:32:30 +0200

    Move and rename inital test case

--- src/test/java/toolsPython/TestConversion.java
+++ src/test/java/toolsPython/TestConversion.java
@@ -1,20 +1,20 @@
+package toolsPython;
+
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.spdx.tools.SpdxToolsHelper;
 import org.spdx.tools.SpdxVerificationException;
 import org.spdx.tools.Verify;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TestCase {
+public class TestConversion {
     @Test
-    public void simpleTestCase() throws SpdxVerificationException, IOException,
+    public void tagToJsonConversionCreatesValidDocument() throws SpdxVerificationException, IOException,
             InterruptedException {
         // Required steps:
         // pip install ../tools-python/
@@ -23,7 +23,9 @@ public class TestCase {
         // Generate any valid SPDX
         // Example using the Python converter tool
         // Could also call "convertor" directly after tools-python is installed
-        var processBuilder = new ProcessBuilder("python3", "/home/nico/IdeaProjects/tools-python/spdx/cli_tools/convertor.py", "-f", "tag", "/home/nico/IdeaProjects/spdx-testbed/src/test/resources/SPDXTagExample.tag", "-o", "output.json");
+        var processBuilder = new ProcessBuilder("python3", "/home/nico/IdeaProjects/tools-python" +
+                "/spdx/cli_tools/convertor.py", "-f", "tag", "/home/nico/IdeaProjects/spdx" +
+                "-testbed/src/test/resources/SPDXTagExample.tag", "-o", "output.json");
         processBuilder.redirectErrorStream(true);
 
         Process process = processBuilder.start();
@@ -32,7 +34,8 @@ public class TestCase {
         var output = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
 
         int exitCode = process.waitFor();
-        assertThat(exitCode).withFailMessage("Received nonzero exit code. Stdout: " + output).isEqualTo(0);
+        assertThat(exitCode).withFailMessage("Received nonzero exit code. Stdout: " + output)
+                .isEqualTo(0);
 
         Path outputFilepath = Path.of("output.json");
         SpdxToolsHelper.SerFileType fileType = SpdxToolsHelper.SerFileType.JSON;
