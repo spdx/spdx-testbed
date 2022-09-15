c4d3c4f10227434692026a18098d49d9efb89691
Maximilian Huber <gh@maxhbr.de>
2022-09-15 13:27:31 +0200

    add wrapper script for spdx/tools-java
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ tools/spdx-tools-java.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version=1.1.0
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cachedir="$script_dir/spdx-tools-java.sh-cache/"
+jar="$cachedir/tools-java-${version}-jar-with-dependencies.jar"
+
+getJarIfNotInCache() (
+    if [[ -f "$jar" ]]; then
+        >&2 echo "$jar already downloaded"
+    else
+        local url="https://github.com/spdx/tools-java/releases/download/v${version}/tools-java-${version}.zip"
+        tmpdir="$(mktemp -d)"
+        cd "$tmpdir"
+        wget \
+            -nc \
+            -O "$tmpdir/tools-java-$version.zip" \
+            "$url" || echo "already existing"
+        7z x -y "$tmpdir/tools-java-$version.zip"
+
+        mkdir -p "$cachedir"
+        cp "$tmpdir/$(basename "$jar")" "$jar"
+        rm -r "$tmpdir"
+    fi
+)
+
+getJarIfNotInCache
+
+set -x
+exec java -jar "$jar" "$@"
