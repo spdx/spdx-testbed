37c0793fc96724b9b41501a1b477c421d9a3702d
Armin Tänzer <armintaenzer@tngtech.com>
2022-09-20 12:26:23 +0200

    clean up the validator script and its output
    Signed-off-by: Armin Tänzer <armintaenzer@tngtech.com>

--- tools/SPDXValidatorScript.py
+++ tools/SPDXValidatorScript.py
@@ -1,46 +1,42 @@
 # script that calls multiple SPDX validation tools on every input file from the command line
-# note: this needs tools-python installed in the environment
 import os
 import sys
 from subprocess import Popen, PIPE
 
-
-def callProcessAndGetOutput(args):
-    newLineCharacter = '\n'.encode()
+def call_process_and_get_output(args):
+    new_line_character = '\n'.encode()
 
     process = Popen(args, stdout=PIPE, stderr=PIPE)
-    scriptOutput = []
+    script_output = []
     stdout, stderr = process.communicate()
-    scriptOutput += stdout.split(newLineCharacter)
+    script_output += stdout.split(new_line_character)
     if stderr != '':
-        scriptOutput += stderr.split(newLineCharacter)
+        script_output += stderr.split(new_line_character)
 
-    return [byte.decode('UTF-8') for byte in scriptOutput if byte != b'']
+    return [byte.decode('UTF-8') for byte in script_output if byte != b'']
 
+def erase_unnecessary_warnings_in_output(output):
+    output.remove("WARNING: sun.reflect.Reflection.getCallerClass is not supported. This will impact performance.")
 
-def validateFile(pathOfFileToValidate, toolsWithArguments):
-    print('\nStarting validation for file ' + pathOfFileToValidate)
+def validate_file(path_of_file_to_validate, tools_with_arguments_intern):
+    print('\nStarting validation for file ' + path_of_file_to_validate)
 
-    for tool, args in toolsWithArguments.items():
-        validationOutput = callProcessAndGetOutput (args + [pathOfFileToValidate])
-        print('\nValidation Result from ' + tool + ':\n')
+    for tool, args in tools_with_arguments_intern.items():
+        validation_output = call_process_and_get_output(args + [path_of_file_to_validate])
+        print('\n### ' + tool + ':\n')
 
-        # prevent the tools-python parser from printing out the whole document
-        if validationOutput[0].startswith('doc comment'):
-            print('Document has been successfully parsed.')
-        else:
-            for resultString in validationOutput:
-                print(resultString)
+        erase_unnecessary_warnings_in_output(validation_output)
+
+        for result_string in validation_output:
+            print(result_string)
 
 
 if __name__ == '__main__':
 
     scriptPath = os.path.dirname(__file__)
 
-    toolsWithArguments = {}
-    toolsWithArguments['new java-tools'] = [os.path.join(scriptPath, 'spdx-tools-java.sh'), 'Verify']
-    toolsWithArguments['old java-tools'] = [os.path.join(scriptPath, 'spdx-tools.sh'), 'Verify']
-#     toolsWithArguments['python-tools'] = ['python3', '-m' 'tools-python.parser']
+    tools_with_arguments = {'new java-tools': [os.path.join(scriptPath, 'spdx-tools-java.sh'), 'Verify'],
+                            'old java-tools': [os.path.join(scriptPath, 'spdx-tools.sh'), 'Verify']}
 
     for filepath in sys.argv[1:]:
-        validateFile(filepath, toolsWithArguments)
+        validate_file(filepath, tools_with_arguments)
