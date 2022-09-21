66ce64701a8ea08dffab0477c91543f27871266f
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-09-21 16:18:06 +0200

    First version of a conversion test case
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

--- src/test/java/conversion/TestConversion.java
+++ src/test/java/conversion/TestConversion.java
@@ -1,12 +1,17 @@
-package toolsPython;
+package conversion;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.spdx.library.InvalidSPDXAnalysisException;
+import org.spdx.tools.InvalidFileNameException;
+import org.spdx.tools.SpdxConverter;
+import org.spdx.tools.SpdxConverterException;
 import org.spdx.tools.SpdxToolsHelper;
 import org.spdx.tools.SpdxVerificationException;
 import org.spdx.tools.Verify;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
@@ -16,7 +21,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestConversion {
     @Disabled("convertor.py is hardcoded, needs to be changed later")
     @Test
-    public void tagToJsonConversionCreatesValidDocument() throws SpdxVerificationException, IOException,
+    public void tagToJsonConversionCreatesValidDocument() throws SpdxVerificationException,
+            IOException,
             InterruptedException {
         // Required steps:
         // pip install ../tools-python/
@@ -45,5 +51,35 @@ public class TestConversion {
         // Verify the output
         var verificationErrors = Verify.verify(outputFilepath.toString(), fileType);
         assertThat(verificationErrors).isEmpty();
+    }
+
+    /**
+     * Test that conversion from tag to json format works.
+     * The converted file is expected
+     */
+    @Test
+    public void convertTagToJson() throws IOException, InvalidSPDXAnalysisException,
+            InvalidFileNameException, SpdxConverterException {
+        var inputFile = new File("SPDXDocumentExamples/tools-java/SPDXTagExample-v2.3.spdx");
+
+        /*
+         This should be handled by the user script
+         */
+        var convertedFile = new File("SPDXDocumentExamples/temp/convertedFile.json");
+        // Just some convenient cleanup
+        convertedFile.delete();
+        convertedFile.getParentFile().mkdirs();
+        SpdxConverter.convert(inputFile.getPath(), convertedFile.getPath());
+
+
+        var inputDocument = SpdxToolsHelper.deserializeDocument(inputFile);
+        var outputDocument = SpdxToolsHelper.deserializeDocument(convertedFile);
+        assertThat(outputDocument.verify()).isEmpty();
+
+        // Covers documentUri and id
+        assertThat(inputDocument).isEqualTo(outputDocument);
+        // Covers all other properties (hopefully...)
+        assertThat(inputDocument.equivalent(outputDocument)).isTrue();
+
     }
 }
