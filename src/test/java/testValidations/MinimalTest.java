1f7691d6697baf22c6734b012d5afba977a8b9b3
Armin Tänzer <armintaenzer@tngtech.com>
2022-09-22 14:57:24 +0200

    add builders for first examples
    Signed-off-by: Armin Tänzer <armintaenzer@tngtech.com>

--- src/test/java/testValidations/MinimalTest.java
+++ src/test/java/testValidations/MinimalTest.java
@@ -1,32 +1,61 @@
 package testValidations;
 
 import org.junit.jupiter.api.Test;
+import org.spdx.jacksonstore.MultiFormatStore;
 import org.spdx.library.InvalidSPDXAnalysisException;
+import org.spdx.library.ModelCopyManager;
 import org.spdx.library.SpdxConstants;
-import org.spdx.library.model.SimpleUriValue;
-import org.spdx.library.model.SpdxDocument;
-import org.spdx.library.model.TypedValue;
+import org.spdx.library.Version;
+import org.spdx.library.model.*;
 import org.spdx.library.model.enumerations.ChecksumAlgorithm;
+import org.spdx.library.model.license.AnyLicenseInfo;
+import org.spdx.library.model.license.LicenseInfoFactory;
+import org.spdx.storage.ISerializableModelStore;
+import org.spdx.storage.simple.InMemSpdxStore;
 import org.spdx.storage.simple.StoredTypedItem;
 import org.spdx.tools.InvalidFileNameException;
-import org.spdx.tools.SpdxToolsHelper;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MinimalTest {
 
+    private SpdxDocument buildMinimalExample() throws InvalidSPDXAnalysisException {
+        ISerializableModelStore modelStore = new MultiFormatStore(new InMemSpdxStore(), MultiFormatStore.Format.JSON_PRETTY);
+        String documentUri = "some_namespace";
+        ModelCopyManager copyManager = new ModelCopyManager();
+
+        //createSpdxDocument already sets creationInfo, dataLicense and specVersion to default values
+        SpdxDocument document = SpdxModelFactory.createSpdxDocument(modelStore, documentUri, copyManager);
+
+        document.setSpecVersion(Version.TWO_POINT_TWO_VERSION);
+        document.setDataLicense(LicenseInfoFactory.parseSPDXLicenseString("CC0-1.0"));
+        document.setCreationInfo(document.createCreationInfo(
+                List.of("Tool: test-tool"), "2022-01-01T00:00:00Z"));
+        document.setName("SPDX-tool-test");
+
+        Checksum sha1Checksum = Checksum.create(modelStore, documentUri, ChecksumAlgorithm.SHA1, "d6a770ba38583ed4bb4525bd96e50461655d2758");
+        AnyLicenseInfo concludedLicense = LicenseInfoFactory.parseSPDXLicenseString("LGPL-3.0-only");
+        SpdxFile file = document.createSpdxFile("SPDXRef-somefile", "./foo.txt", concludedLicense,
+                        List.of(), "Copyright 2022 some guy", sha1Checksum)
+                .build();
+
+        document.getDocumentDescribes().add(file);
+
+        return document;
+    }
+
     @Test
     public void correctMinimalExample() throws IOException, InvalidFileNameException {
-        String filepath = "/home/armin/IdeaProjects/spdx-testbed/SPDXDocumentExamples/playgroundFiles/SPDXXML_minimalExample-v2.2.spdx.xml";
-        File spdxDocument = new File(filepath);
+        //String filepath = "/home/armin/IdeaProjects/spdx-testbed/SPDXDocumentExamples/playgroundFiles/SPDXXML_minimalExample-v2.2.spdx.xml";
+        //File spdxDocument = new File(filepath);
 
         try {
-            SpdxDocument doc = SpdxToolsHelper.deserializeDocument(spdxDocument);
-
+            //SpdxDocument doc = SpdxToolsHelper.deserializeDocument(spdxDocument);
+            SpdxDocument doc = buildMinimalExample();
             assertThat(doc.verify()).isEmpty();
 
             assertThat(doc.getId()).isEqualTo("SPDXRef-DOCUMENT");
