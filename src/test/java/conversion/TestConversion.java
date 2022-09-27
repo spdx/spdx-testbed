1f30437b15b9d2119747bd1714f29e758e074870
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-09-27 11:48:58 +0200

    Remove python-tools conversion test (future plan is to require users to write scripts calling their tools in our CI pipeline, so in the test we expect the finished file to be provided)
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

--- src/test/java/conversion/TestConversion.java
+++ src/test/java/conversion/TestConversion.java
@@ -1,59 +1,19 @@
 package conversion;
 
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.spdx.library.InvalidSPDXAnalysisException;
 import org.spdx.tools.InvalidFileNameException;
 import org.spdx.tools.SpdxConverter;
 import org.spdx.tools.SpdxConverterException;
 import org.spdx.tools.SpdxToolsHelper;
-import org.spdx.tools.SpdxVerificationException;
-import org.spdx.tools.Verify;
 import util.Comparisons;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestConversion {
-    @Disabled("convertor.py is hardcoded, needs to be changed later")
-    @Test
-    public void tagToJsonConversionCreatesValidDocument() throws SpdxVerificationException,
-            IOException,
-            InterruptedException {
-        // Required steps:
-        // pip install ../tools-python/
-        // (this means the CLI tool "convertor" is available in your global python installation...)
-
-        // Generate any valid SPDX
-        // Example using the Python converter tool
-        // Could also call "convertor" directly after tools-python is installed
-        var processBuilder = new ProcessBuilder("python3", "/home/nico/IdeaProjects/tools-python" +
-                "/spdx/cli_tools/convertor.py", "-f", "tag", "/home/nico/IdeaProjects/spdx" +
-                "-testbed/src/test/resources/SPDXTagExample.tag", "-o", "output.json");
-        processBuilder.redirectErrorStream(true);
-
-        Process process = processBuilder.start();
-
-        // Could include errors, helpful for debugging
-        var output = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
-
-        int exitCode = process.waitFor();
-        assertThat(exitCode).withFailMessage("Received nonzero exit code. Stdout: " + output)
-                .isEqualTo(0);
-
-        Path outputFilepath = Path.of("output.json");
-        SpdxToolsHelper.SerFileType fileType = SpdxToolsHelper.SerFileType.JSON;
-
-        // Verify the output
-        var verificationErrors = Verify.verify(outputFilepath.toString(), fileType);
-        assertThat(verificationErrors).isEmpty();
-    }
-
     /**
      * Test that conversion from tag to json format works.
      * The converted file is expected to be provided by the user
