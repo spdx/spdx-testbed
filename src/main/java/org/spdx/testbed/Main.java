bce348d934b675c21881c525c332c253dfe0d28c
Armin Tänzer <armin.taenzer@tngtech.com>
2022-10-04 11:29:36 +0200

    add uberJar functionality, fix some logic issues in Main
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/main/java/org/spdx/testbed/Main.java
+++ src/main/java/org/spdx/testbed/Main.java
@@ -10,27 +10,36 @@ import java.io.IOException;
 public class Main {
     //TODO: maybe exceptions should be caught?
     public static void main(String[] args) throws IOException, InvalidSPDXAnalysisException, InvalidFileNameException {
-        if (args.length > 2) {
+        //TODO: catch wrong number of arguments
+        if (args.length > 1) {
             var testCase = args[0];
             var remainingArgs = ArrayUtils.remove(args, 0);
 
             switch (testCase) {
                 case "generationMinimalTest":
                     (new GenerationMinimalTestCase()).test(remainingArgs);
+                    break;
                 case "generationDocumentTest":
                     (new GenerationDocumentTestCase()).test(remainingArgs);
+                    break;
                 case "generationPackageTest":
                     (new GenerationPackageTestCase()).test(remainingArgs);
+                    break;
                 case "generationFileTest":
                     (new GenerationFileTestCase()).test(remainingArgs);
+                    break;
                 case "generationSnippetTest":
                     (new GenerationSnippetTestCase()).test(remainingArgs);
+                    break;
                 case "generationLicenseTest":
                     (new GenerationLicenseTestCase()).test(remainingArgs);
+                    break;
                 case "generationRelationshipTest":
                     (new GenerationRelationshipTestCase()).test(remainingArgs);
+                    break;
                 case "generationExtractedLicenseInfoTest":
                     (new GenerationExtractedLicenseInfoTestCase()).test(remainingArgs);
+                    break;
             }
         }
     }
