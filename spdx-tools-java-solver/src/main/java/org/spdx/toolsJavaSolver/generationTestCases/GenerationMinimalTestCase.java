2f9956ff67800d4989b3a033e50b840d899c52f2
Armin Tänzer <armin.taenzer@tngtech.com>
2022-10-06 15:48:59 +0200

    add minimal task description, adapt minimal test to v2.3
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- spdx-tools-java-solver/src/main/java/org/spdx/toolsJavaSolver/generationTestCases/GenerationMinimalTestCase.java
+++ spdx-tools-java-solver/src/main/java/org/spdx/toolsJavaSolver/generationTestCases/GenerationMinimalTestCase.java
@@ -2,7 +2,6 @@ package org.spdx.toolsJavaSolver.generationTestCases;
 
 import org.spdx.library.InvalidSPDXAnalysisException;
 import org.spdx.library.model.SpdxDocument;
-import org.spdx.library.model.license.LicenseInfoFactory;
 
 import java.util.List;
 
@@ -12,8 +11,7 @@ public class GenerationMinimalTestCase {
         var document = GenerationUtil.createSpdxDocumentWithBasicInfo("Minimal test document");
 
         var sha1Checksum = GenerationUtil.createSha1Checksum(document.getModelStore(), document.getDocumentUri());
-        var concludedLicense = LicenseInfoFactory.parseSPDXLicenseString("LGPL-3.0-only");
-        var file = document.createSpdxFile("SPDXRef-somefile", "./foo.txt", concludedLicense, List.of(), "Copyright 2022 some guy", sha1Checksum).build();
+        var file = document.createSpdxFile("SPDXRef-somefile", "./foo.txt", null, List.of(), null, sha1Checksum).build();
 
         document.getDocumentDescribes().add(file);
 
--- testbed/src/main/java/org/spdx/testbed/generationTestCases/GenerationMinimalTestCase.java
+++ testbed/src/main/java/org/spdx/testbed/generationTestCases/GenerationMinimalTestCase.java
@@ -2,7 +2,6 @@ package org.spdx.testbed.generationTestCases;
 
 import org.spdx.library.InvalidSPDXAnalysisException;
 import org.spdx.library.model.SpdxDocument;
-import org.spdx.library.model.license.LicenseInfoFactory;
 
 import java.util.List;
 
@@ -15,8 +14,7 @@ public class GenerationMinimalTestCase extends GenerationTestCase {
         var documentUri = document.getDocumentUri();
 
         var sha1Checksum = createSha1Checksum(modelStore, documentUri);
-        var concludedLicense = LicenseInfoFactory.parseSPDXLicenseString("LGPL-3.0-only");
-        var file = document.createSpdxFile("SPDXRef-somefile", "./foo.txt", concludedLicense, List.of(), "Copyright 2022 some guy", sha1Checksum).build();
+        var file = document.createSpdxFile("SPDXRef-somefile", "./foo.txt", null, List.of(), null, sha1Checksum).build();
 
         document.getDocumentDescribes().add(file);
 
