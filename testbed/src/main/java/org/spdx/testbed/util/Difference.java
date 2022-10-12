9c8a22194f2b5eff43d49f6386c341573eb483c3
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-10-12 14:24:57 +0200

    Add basic tests for new comparison method
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

--- testbed/src/main/java/org/spdx/testbed/util/Difference.java
+++ testbed/src/main/java/org/spdx/testbed/util/Difference.java
@@ -3,6 +3,7 @@ package org.spdx.testbed.util;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,6 +13,7 @@ import lombok.ToString;
 @Builder
 @AllArgsConstructor
 @ToString
+@EqualsAndHashCode
 public class Difference {
     private JsonNode firstValue;
     private JsonNode secondValue;
