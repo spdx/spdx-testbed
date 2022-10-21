324ccde0329a57ad1612f713e4f06996f3f09b8d
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-10-21 09:12:12 +0200

    [refactor] Separate parsing the CLI input from processing it
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

--- testbed/src/main/java/org/spdx/testbed/Main.java
+++ testbed/src/main/java/org/spdx/testbed/Main.java
@@ -4,6 +4,7 @@
 
 package org.spdx.testbed;
 
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
@@ -36,66 +37,68 @@ public class Main {
                 .build());
 
         var parser = new DefaultParser();
+        CommandLine cmd;
 
         try {
-            var cmd = parser.parse(options, args);
-
-            if (cmd.hasOption("h")) {
-                printUsage(options);
-                System.exit(0);
-            }
-
-            if (!cmd.hasOption("t") && !cmd.hasOption("c")) {
-                // TODO: Customize help message to mention that at least one of t or c is 
-                //  required (but specifying both is possible!)
-                printUsage(options);
-                System.exit(1);
-            }
-
-            var testCaseFinder = new TestCaseFinder();
-
-            var testCasesByNameOptional = Optional.ofNullable(cmd.getOptionValue("t"))
-                    .map(testcaseName -> testCaseFinder.findTestCasesByNames(List.of(testcaseName)));
-            var testCasesByCategoriesOptional = Optional.ofNullable(cmd.getOptionValues("c"))
-                    .map(categoriesArray -> Arrays.stream(categoriesArray)
-                            .map(TestCaseCategory::fromString)
-                            .collect(Collectors.toList()))
-                    .map(testCaseFinder::findTestCasesByCategories);
-            var files = cmd.getOptionValues("f");
-
-            var selectedTestCases = new ArrayList<TestCase>();
-            if (testCasesByNameOptional.isPresent() && testCasesByCategoriesOptional.isPresent()) {
-                selectedTestCases.addAll(testCasesByNameOptional.get());
-                selectedTestCases.retainAll(testCasesByCategoriesOptional.get());
-            } else if (testCasesByNameOptional.isPresent()) {
-                selectedTestCases.addAll(testCasesByNameOptional.get());
-            } else {
-                testCasesByCategoriesOptional.ifPresent(selectedTestCases::addAll);
-            }
-
-            // Alphabetical sort to have a well-defined order for matching the files
-            selectedTestCases.sort(TestCase::compareTo);
-
-            if (selectedTestCases.size() != files.length) {
-                System.err.println("The number of input files does not match the number of " +
-                        "selected test cases. " + files.length + " input files were provided, but" +
-                        " " + selectedTestCases.size() + " test cases were selected:");
-                selectedTestCases.forEach(innerTestCase -> System.err.println(innerTestCase.getName()));
-                System.exit(1);
-            }
-
-            // TODO: do something with the test results
-            for (int i = 0; i < selectedTestCases.size(); i++) {
-                var inputFile = new String[]{files[i]};
-                selectedTestCases.get(i).test(inputFile);
-            }
-
+            cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.err.println(e.getMessage());
 
             printUsage(options);
             System.exit(1);
+            return;
         }
+
+        if (cmd.hasOption("h")) {
+            printUsage(options);
+            System.exit(0);
+        }
+
+        if (!cmd.hasOption("t") && !cmd.hasOption("c")) {
+            // TODO: Customize help message to mention that at least one of t or c is 
+            //  required (but specifying both is possible!)
+            printUsage(options);
+            System.exit(1);
+        }
+
+        var testCaseFinder = new TestCaseFinder();
+
+        var testCasesByNameOptional = Optional.ofNullable(cmd.getOptionValue("t"))
+                .map(testcaseName -> testCaseFinder.findTestCasesByNames(List.of(testcaseName)));
+        var testCasesByCategoriesOptional = Optional.ofNullable(cmd.getOptionValues("c"))
+                .map(categoriesArray -> Arrays.stream(categoriesArray)
+                        .map(TestCaseCategory::fromString)
+                        .collect(Collectors.toList()))
+                .map(testCaseFinder::findTestCasesByCategories);
+        var files = cmd.getOptionValues("f");
+
+        var selectedTestCases = new ArrayList<TestCase>();
+        if (testCasesByNameOptional.isPresent() && testCasesByCategoriesOptional.isPresent()) {
+            selectedTestCases.addAll(testCasesByNameOptional.get());
+            selectedTestCases.retainAll(testCasesByCategoriesOptional.get());
+        } else if (testCasesByNameOptional.isPresent()) {
+            selectedTestCases.addAll(testCasesByNameOptional.get());
+        } else {
+            testCasesByCategoriesOptional.ifPresent(selectedTestCases::addAll);
+        }
+
+        // Alphabetical sort to have a well-defined order for matching the files
+        selectedTestCases.sort(TestCase::compareTo);
+
+        if (selectedTestCases.size() != files.length) {
+            System.err.println("The number of input files does not match the number of " +
+                    "selected test cases. " + files.length + " input files were provided, but" +
+                    " " + selectedTestCases.size() + " test cases were selected:");
+            selectedTestCases.forEach(innerTestCase -> System.err.println(innerTestCase.getName()));
+            System.exit(1);
+        }
+
+        // TODO: do something with the test results
+        for (int i = 0; i < selectedTestCases.size(); i++) {
+            var inputFile = new String[]{files[i]};
+            selectedTestCases.get(i).test(inputFile);
+        }
+
     }
 
     private static void printUsage(Options options) {
